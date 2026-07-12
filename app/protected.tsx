c2f5f8223b110841bbf024bd59ebf11bc3e58663
Iulian Gorincioi <iulik.g07@gmail.com>
2026-07-12 15:40:03 +0200

    Fix SplashScreen

--- app/protected.tsx
+++ app/protected.tsx
@@ -13,20 +13,24 @@ export default function Protected({
 }) {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
+  const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       if (!user) {
         router.replace("/");
       } else {
-        setLoading(false);
+        setTimeout(() => {
+  setLoading(false);
+  setShowSplash(false);
+}, 1500);
       }
     });
 
     return () => unsub();
   }, [router]);
 
-     if (loading) {
+     if (loading || showSplash) {
   return <SplashScreen />;
 }
 
